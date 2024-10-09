ac828b09df0f8866bf327877e7962763f1960de4
Ankit Kumar Singh <107400903+ankit-t12@users.noreply.github.com>
2024-10-09 22:56:42 +0530

    Initial commit

--- src/Oct/ex_03_10_2024/Lab001.java
+++ src/Oct/ex_03_10_2024/Lab001.java
@@ -4,5 +4,6 @@ public class Lab001 {
     public static void main(String[] args) {
         System.out.println("!!!hello world!!!");
         System.out.println("!!!hello multiverse !!!");
+        System.out.println("!!!hello al  multiverse !!!");
     }
 }
